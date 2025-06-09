51634755e9a2b5ab1d1d7375b9fe4d3c212629b0
剑出樱花起 <15234597+sword-out-cherry-blossoms-rise@user.noreply.gitee.com>
2025-06-09 09:16:42 +0800

    1

--- xm/src/main/java/com/xty/App.java
+++ xm/src/main/java/com/xty/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "乌拉乌拉乌拉" );
+        System.out.println( "乌拉乌拉乌拉" );
+        System.out.println( "乌拉乌拉乌拉" );
+        System.out.println( "乌拉乌拉乌拉" );
+        System.out.println( "乌拉乌拉乌拉" );
+        System.out.println( "乌拉乌拉乌拉" );System.out.println( "乌拉乌拉乌拉" );
+        System.out.println( "乌拉乌拉乌拉" );
+
+
     }
 }
